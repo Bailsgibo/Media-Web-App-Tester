8e8f460026b54c143fba71c6cf58c14de73f314a
Bailsgibo1 <78455196+Bailsgibo@users.noreply.github.com>
2021-08-29 21:47:07 +0000

    Nav Links Broken

---
+++ auth.js
@@ -0,0 +1,63 @@
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+        db.collection('boards').onSnapshot(snapshot => {
+            setupBoards(snapshot.docs);
+            setupUI(user);
+        }, err => console.log(err.message));
+        var uid = user.uid;
+        var user = firebase.auth().currentUser;
+        document.getElementById("divmyUser").innerHTML = user.uid;
+    } else {
+        //signed out
+    }
+});
+
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    // sign up the user & add firestore data
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        return db.collection('users').doc(cred.user.uid).set({
+            bio: signupForm['signup-bio'].value
+        });
+    }).then(() => {
+        // close the signup modal & reset form
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+    const bio = signupForm['signup-bio'].value;
+
+    // log the user in
+    auth.signInWithEmailAndPassword(email, password).then((cred) => {
+        // close the signup modal & reset form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+
+});
--- index.js
+++ index.js
@@ -1,53 +1,65 @@
-function login() {
+// DOM elements
+// document.getelementbyid("").innerhtml = uid
+const boardList = document.querySelector('.boards');
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+const accountDetails = document.querySelector('.account-details');
 
-    var userEmail = document.getElementById("email_field").value;
-    var userPassword = document.getElementById("password_field").value;
+//retrieve one document and save it to userDetails
 
-    firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).then((userCredential) => {
-            // Signed in
-            window.alert("Signing in...")
-            var user = userCredential.user;
-            // ...
-        window.location = 'index.html';
-        })
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            window.alert("Error1: " + errorMessage);
+const setupUI = (user) => {
+    if (user) {
+        // account info
+        db.collection('users').doc(user.uid).get().then(doc => {
+            const html = `
+        <div>User ID: ${user.uid}</div>
+        <div>Logged in as: ${user.email}</div>
+        <div>Username: ${user.bio}</div>
+      `;
+            accountDetails.innerHTML = html;
         });
+        // toggle user UI elements
+        loggedInLinks.forEach(item => item.style.display = 'block');
+        loggedOutLinks.forEach(item => item.style.display = 'none');
+    } else {
+        // clear account info
+        accountDetails.innerHTML = '';
+        // toggle user elements
+        loggedInLinks.forEach(item => item.style.display = 'none');
+        loggedOutLinks.forEach(item => item.style.display = 'block');
+    }
+};
 
-}
+// setup boards
+const setupBoards = (data) => {
 
-function logout() {
-
-    firebase.auth().signOut().then(() => {
-        // Sign-out successful.
-        window.alert("LOGOUT SUCCESFUL")
-        window.location = 'login.html';
-    }).catch((error) => {
-        // An error happened.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        window.alert("Error2: " + errorMessage)
-
-    });
-
-
-}
-
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/firebase.User
-            var uid = user.uid;
-            var user = firebase.auth().currentUser;
+    if (data.length) {
+        let html = '';
+        data.forEach(doc => {
+            const board = doc.data();
+            const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4"> ${board.title} </div>
+          <div class="collapsible-body white"> ${board.content} </div>
+        </li>
+      `;
+            html += li;
+        });
+        boardList.innerHTML = html;
+    } else {
+        boardList.innerHTML = '';
+    }
 
 
-        } else { 
-            // User is signed out
-            // ...
-            window.alert("you are signed out...");
-            //document.getElementById("user_div").style.display = "none";
-            //document.getElementById("login_div").style.display = "block";
-        }
-    });
+};
+
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function () {
+
+    var modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
+
+    var items = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(items);
+
+});
